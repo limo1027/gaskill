b276044e6391c72fb1e776daffadf94814a4a9ec
limo1027 <28009663@qq.com>
2026-07-23 14:40:12 +0800

    v2.8.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import os
 from pathlib import Path
 long_description = ""
-VERSION = eval(os.getenv('VERSION', '"2.8.3"'))
+VERSION = eval(os.getenv('VERSION', '"2.8.4"'))
 
 
 def run(cmd, check=True):
